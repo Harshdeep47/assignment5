b5118c43b489a0269d5a55504c11933545326c14
Harshdeep47 <harshdeep080808@gmail.com>
2025-09-14 21:29:10 +0530

    Add files via upload

---
+++ a5_q3_Harshdeep;1024030703.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+};
+
+Node* head = NULL;
+
+void insertEnd(int val) {
+    Node* newNode = new Node();
+    newNode->data = val;
+    newNode->next = NULL;
+    if(head == NULL) {
+        head = newNode;
+        return;
+    }
+    Node* temp = head;
+    while(temp->next != NULL) temp = temp->next;
+    temp->next = newNode;
+}
+
+void findMiddle() {
+    Node* slow = head;
+    Node* fast = head;
+    while(fast != NULL && fast->next != NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    if(slow != NULL) cout << slow->data << endl;
+}
+
+int main() {
+    int n, val;
+    cin >> n;
+    for(int i=0;i<n;i++) {
+        cin >> val;
+        insertEnd(val);
+    }
+    findMiddle();
+    return 0;
+}
+
