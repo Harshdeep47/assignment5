a0f975a52aec190260520415c6bdd42aee984290
Harshdeep47 <harshdeep080808@gmail.com>
2025-09-14 21:29:37 +0530

    Add files via upload

---
+++ a5_q4_Harshdeep;1024030703.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+};
+
+Node* head = NULL;
+
+void insertEnd(int val) {
+    Node* newNode = new Node();
+    newNode->data = val;
+    newNode->next = NULL;
+    if(head == NULL) {
+        head = newNode;
+        return;
+    }
+    Node* temp = head;
+    while(temp->next != NULL) temp = temp->next;
+    temp->next = newNode;
+}
+
+void display() {
+    Node* temp = head;
+    while(temp != NULL) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+void reverse() {
+    Node* prev = NULL;
+    Node* curr = head;
+    Node* next = NULL;
+    while(curr != NULL) {
+        next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+    }
+    head = prev;
+}
+
+int main() {
+    int n, val;
+    cin >> n;
+    for(int i=0;i<n;i++) {
+        cin >> val;
+        insertEnd(val);
+    }
+    reverse();
+    display();
+    return 0;
+}
+
